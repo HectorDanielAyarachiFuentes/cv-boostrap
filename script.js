73ff7a74206445046cf8b1cfb8420267b2a87ac4
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2024-02-19 20:57:24 -0300

    123

--- script.js
+++ script.js
@@ -11,3 +11,29 @@
             audioPlayer.pause();
         }
     });
+
+
+     // Para descargar en formato PDF
+
+     function downloadPDF() {
+        // Load print-optimized CSS
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'print-style.css';
+        link.media = 'all';
+
+        // Clone the body and apply the print stylesheet
+        var body = document.body.cloneNode(true);
+        body.appendChild(link);
+        
+        var opt = {
+            margin:       [15, 15, 15, 15],
+            filename:     'CV-Hector Daniel-Ayarachi Fuentes.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        };
+
+        html2pdf().from(body).set(opt).save();
+    }
